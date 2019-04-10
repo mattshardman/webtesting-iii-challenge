459faf8771c4aa8911c44898098ae5d5dd8e8ce7
mattshardman <mattshardman@gmail.com>
2019-04-10 14:19:35 +0100

    add test to check when both close and locked are clicked

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,6 +3,8 @@ import React from 'react';
 import * as rt from 'react-testing-library';
 import Dashboard from './Dashboard';
 
+afterEach(rt.cleanup);
+
 describe('check default', () => {
     it('defaults to unlocked and open', () => {
         const { getByText } = rt.render(<Dashboard />);
@@ -11,15 +13,51 @@ describe('check default', () => {
         expect(unlocked);
         expect(open);
     });
+
+    it('lock gate button is disabled', () => {
+        const { getByText } = rt.render(<Dashboard />);
+        const lockButton = getByText(/lock gate/i);
+        expect(lockButton.disabled).toBeTruthy();
+    });
 });
 
 describe('buttons functionality',  () => {
     it('displays closed when close gate is clicked', () => {
         const { getByText } = rt.render(<Dashboard />);
         const closeButton = getByText(/close gate/i);
-        rt.fireEvent.click(closeButton, (i) => console.log(i));
+        rt.fireEvent.click(closeButton);
         const closed = getByText('Closed');
         expect(closed)
+    });
+
+    it('lock gate is not disabled when gate is closed', () => {
+        const { getByText } = rt.render(<Dashboard />);
+        const closeButton = getByText(/close gate/i);
+        const lockButton = getByText(/lock gate/i);
+        rt.fireEvent.click(closeButton);
+        expect(lockButton.disabled).toBeFalsy();
+    });
+
+    it('displays locked if close and then locked are clicked', () => {
+        const { getByText } = rt.render(<Dashboard />);
+        const closeButton = getByText(/close gate/i);
+        const lockButton = getByText(/lock gate/i);
+        rt.fireEvent.click(closeButton);
+        rt.fireEvent.click(lockButton);
+
+        const locked = getByText('Locked');
+        expect(locked)
+    });
+
+    it('open gate is disabled if gate is locked', () => {
+        const { getByText } = rt.render(<Dashboard />);
+        const closeButton = getByText(/close gate/i);
+        const lockButton = getByText(/lock gate/i);
+        rt.fireEvent.click(closeButton);
+        rt.fireEvent.click(lockButton);
+
+        const openButton = getByText(/open gate/i);
+        expect(openButton.disabled).toBeTruthy();
     });
 });
 
